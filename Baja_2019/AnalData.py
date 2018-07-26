6fbb50cf8a2e1a18e672c51cb01a5327916dcac0
Clay Freeman <freeman.clayton@gmail.com>
2018-07-26 00:59:17 -0500

    newstuff

--- Baja_2019/AnalData.py
+++ Baja_2019/AnalData.py
@@ -1,9 +1,27 @@
+#! /usr/bin/env python3
 import os
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import math as math
-from scipy import signal, stats
+
 
 def main():
+    for filename in os.listdir('.'):
+        if filename.startswith("ArduinoPoop"):
+            print("HEY")
+            pt, a0, a1, a2, c_time, wt = np.loadtxt(open(filename),
+                                                    delimiter=',',
+                                                    skiprows=1,
+                                                    unpack=True)
+            c_time = c_time/1000
+            plt.plot(c_time, a0,label='Analog Pin 0')
+            plt.plot(c_time, a1,label='Analog Pin 1')
+            plt.plot(c_time, a2,label='Analog Pin 2')
+            plt.xlabel('Time (s)')
+            plt.ylabel('Reading')
+            plt.legend()
+            plt.show()
 
+main()
 
