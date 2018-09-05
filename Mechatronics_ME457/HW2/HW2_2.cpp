3136c77381e4334264efc2966f32fb78c1f51243
Clay Freeman - PC <freeman.clayton@gmail.com>
2018-09-05 13:24:31 -0500

    update 0905-02

--- Mechatronics_ME457/HW2/HW2_2.cpp
+++ Mechatronics_ME457/HW2/HW2_2.cpp
@@ -1,27 +1,35 @@
 /*
 ME457 HW2 Problem 2
-
-Let time be the x-axis or step size
-
-Let pitch_rate be the reading in rad/s from the gyro
 */
+// initialize libraries
 #include <stdio.h>
-#include <math.h>
-
-float t_times[] = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9};
-float g_data[] = {0.0, 0.1, 0.1, 0.1, 0.2, 0.2, 0.1, 0.1, 0.3, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.1, 0.1, 0.1, 0.1, 0.0, 0.0, -0.1, -0.1, -0.2, -0.2, -0.3, -0.2, -0.1, -0.1};
-
-int d_size = sizeof(g_data)/sizeof(g_data[0]);
-float g_rates[d_size];
+#include <iostream>
+#include <string>
+// define constants
+#define PI 3.14159265
 
 int main()
 {
-    for(int i=0; i < d_size; i++)
-    {
-        if (i == 0)
-        {
-            double hgt = t_times[i+1] - t_times[i];
-            g_rates[i] = ((g_data[i] - g_data[i+1])/2) * hgt;
-        }
-    }
+    // Initial pitch reading
+    double i_pitch = 0;
+    // first gyrometer reading
+    double g_point1 = 0;
+    // second gyrometer reading
+    double g_point2 = 0.1;
+    // first time point
+    double t_point1 = 0;
+    // second time point
+    double t_point2 = 0.1;
+    // current pitch calculation using trapezoidal numeric integration method
+    double current_pitch = i_pitch + ((t_point2 - t_point1)*-1) * ((g_point1 + g_point2)/2) * (180/PI);
+
+    std::cout << "For initial pitch reading : " << i_pitch << std::endl;
+    std::cout << "Initial gyrometer reading : " << g_point1 << std::endl;
+    std::cout << "Final gyrometer reading : " << g_point2 << std::endl;
+    std::cout << "Initial time reading : " << t_point1 << std::endl;
+    std::cout << "Final time reading : " << t_point2 << std::endl;
+    std::cout << "Calculated pitch output : " << current_pitch << std::endl;
+    
+
+
 }
