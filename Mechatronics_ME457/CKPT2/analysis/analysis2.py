71cb8a6af0ba4fda2a03f6851852d8d9614cfb12
Clay Freeman <freeman.clayton@gmail.com>
2018-10-09 19:36:15 -0500

    update_1009_1936

---
+++ Mechatronics_ME457/CKPT2/analysis/analysis2.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np, matplotlib.pyplot as plt
+
+p_files = []
+
+for file_name in os.listdir('.'):
+    if file_name.endswith('_figure.png'):
+        p_files.append(file_name[:-11])
+
+for file_name in os.listdir('.'):
+    if file_name.endswith('csv'):
+        if file_name[:-4] not in p_files:
+            try:
+                d_time, signal, pitch = np.loadtxt(open(file_name),
+                                                delimiter = ',',
+                                                unpack = True,
+                                                skiprows = 1,
+                                                usecols = (1, 38, 39),)
+                d_time /= 1000000
+                pitch += 20
+                plt.plot(d_time[1600:5500], signal[1600:5500], label='Signal')
+                plt.plot(d_time[1600:5500], pitch[1600:5500], label='Pitch')
+                plt.title('Pitch and Response\n{}'.format(file_name))
+                plt.xlabel('time (s)')
+                plt.ylabel('Pitch (Degrees)')
+                plt.legend()
+                plt.savefig('{}_figure.png'.format(file_name[:-4]), bbox_inches='tight')
+                plt.show()
+            except:
+                pass
